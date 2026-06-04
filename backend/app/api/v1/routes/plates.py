e7e2e70d6a28ddb157a46119d7423549ef41792d
gwiazdan <norbert.gwiazda@icloud.com>
2026-06-04 20:32:17 +0200

    feat(backend): add deps and plates GET endpoint

--- backend/app/api/v1/routes/plates.py
+++ backend/app/api/v1/routes/plates.py
@@ -1,3 +1,17 @@
+from typing import Any
+
 from fastapi import APIRouter
+from sqlmodel import select
+
+from app.api.v1.deps import SessionDep
+from app.models import Plate
 
 router = APIRouter(prefix="/plates", tags=["plates"])
+
+
+@router.get("/", response_model=list[Plate])
+def read_plate(session: SessionDep) -> Any:
+    """
+    Get license plate list
+    """
+    return session.exec(select(Plate)).all()
